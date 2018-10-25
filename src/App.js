bf272a53465e1bd4d667ff5ab8619af5f6bc66ef
Malte Sandstede <malte@sandstede.com>
2018-10-25 20:02:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
 
 class App extends Component {
+  constructor(props) {
+    super(props)  
+
+    this.state = { 
+      socket: io('http://localhost:3001'),
+      result: []
+    }
+  }
+
+  componentDidMount() {
+    const { socket } = this.state
+
+    socket.on('status', data => {
+      console.log(data)
+    })
+
+    socket.on('people', result => {
+      this.setState({result: this.state.result.concat(result.new_val)})
+    })
+  }
+
   render() {
+    const { result, socket } = this.state
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        {result.map((r, i) => ( <span key={i}>{r.name}, {r.age} years old, {r.distance}km distance</span> ))}
         </header>
       </div>
     );
   }
+
 }
 
 export default App;
